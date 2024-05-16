cc206cfd39aaab722b36776fdadd67429b80c270
TagoreZhao <tagorezhao@berkeley.edu>
2024-05-17 03:53:33 +0800

    update

---
+++ journal_replication/sharebikedemand/scripts_t/estimation_t.py
@@ -0,0 +1,5 @@
+# We will need to load all libraries here
+
+# We start with section
+def gen_sync_in_grid() #We first need figure out what parameters we are gonna need and output.
+    print("This function is used to generate our syntheyic data.")
