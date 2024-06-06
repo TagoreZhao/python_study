d07d1402949763de8e335bcbe8c4edf04b571240
TagoreZhao <tagorezhao@berkeley.edu>
2024-06-06 10:35:52 -0700

    update

--- journal_replication/sharebikedemand/scripts_t/estimation_t.py
+++ journal_replication/sharebikedemand/scripts_t/estimation_t.py
@@ -1,5 +1,18 @@
 # We will need to load all libraries here
-print("Hello World")
-# We start with section
-#def gen_sync_in_grid() #We first need figure out what parameters we are gonna need and output.
-#    print("This function is used to generate our syntheyic data.")
+import numpy as np
+
+#test code to see if this is actually executable
+#print("Hello World")
+
+# We start with section 5.1, we need define functions that will help us generate the synthetic data.
+
+#We first need figure out what parameters we are gonna need and output.
+#We need to specify the input and output
+'''
+Input:
+    random_seed
+    loc_bound: bound for arrival and destination location. 
+'''
+def gen_sync_in_grid():
+
+    print("This function is used to generate our syntheyic data.")
