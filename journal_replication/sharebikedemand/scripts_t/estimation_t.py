de088a5a260991cf738be7e6d5ecfab0cbddcecc
TagoreZhao <tagorezhao@berkeley.edu>
2024-08-15 21:38:35 -0700

    finish table 1

--- journal_replication/sharebikedemand/scripts_t/estimation_t.py
+++ journal_replication/sharebikedemand/scripts_t/estimation_t.py
@@ -2,7 +2,8 @@
 import numpy as np
 from tqdm import tqdm
 import warnings
-
+import matplotlib.pyplot as plt
+from scipy.optimize import linprog
 
 #test code to see if this is actually executable
 #print("Hello World")
@@ -269,21 +270,14 @@ def findprob0(dist_cur,genpos,beta0,beta1_true):
  '''
   return 1/(1+np.sum(np.exp(beta0+beta1_true*dist_cur[genpos,:])))
 
-def caldist(loc, bike_loc, bike_num):
+def caldist(loc,bike_loc,bike_num):
     '''
-    Input:
-        loc: The location of the rider
-        bike: The location of th bikes
-        bike_num: number of bike
-    Output: 
-        An array that contains the Euclidean distance between arrival locations and bikes at each time period. If bike is unavailable,
-        the distance is infinity.
-
-    loc[:, np.newaxis, np.newaxis, :] expands loc into a shape suitable for broadcasting with bike_loc.
-    bike_loc[np.newaxis, :, :, :] similarly expands bike_loc for broadcasting.
+    Return a numpy array that contains the Euclidean distance between arrival locations and bikes at each time period. If a bike is 
+    unavailable at some time, the distance from any arrival location to the bike at that time is recorded as infinity.
     '''
-    # Broadcasting to calculate the Euclidean distance
-    dist = np.linalg.norm(loc[:, np.newaxis, np.newaxis, :] - bike_loc[np.newaxis, :, :, :], axis=-1)
+    loc = loc.reshape(-1,1,1,2)
+    bike_loc = bike_loc.reshape(1,-1,bike_num,2)
+    dist = np.sqrt(np.sum((loc-bike_loc)**2,axis=3))
     return dist
 
 
@@ -422,3 +416,26 @@ def h(w,choice_prob,all_period,num_booked):
 def find_grad_g(w,choice_prob,book_bike,book_index):
     grad_g = np.sum(choice_prob[:,book_bike+1,book_index]/np.sum(w.reshape(-1,1)*(choice_prob[:,book_bike+1,book_index]),axis=0),axis=1)
     return grad_g
+
+def find_wasserstein(loc1,loc2,w1,w2):
+    '''
+    Compute the second moment Wasserstein distance between two sets of weighted locations.
+    loc1,loc2: location sets that contains the coordinate of each location 
+    w1,w2: weight vectors of the corresponding location set
+    '''
+    w1_len = w1.shape[0]
+    w2_len = w2.shape[0]
+    loc1 = loc1.reshape(1,-1,2)
+    loc2 = loc2.reshape(-1,1,2)
+    dist_mat = (loc1[:,:,0]-loc2[:,:,0])**2+(loc1[:,:,1]-loc2[:,:,1])**2
+    c = dist_mat.reshape(-1)
+    A = np.zeros((w1_len+w2_len,w1_len*w2_len))
+    b = np.concatenate((w1,w2))
+    for i in range(w1_len):
+        A[i,w2_len*i:w2_len*(i+1)] = np.ones(w2_len)
+    for i in range(w2_len):
+        A[i+w1_len,np.linspace(i,(w1_len-1)*w2_len+i,w1_len,dtype=int)] = np.ones(w1_len)
+    lam_bounds = (-2e-7,None)
+    res = linprog(c, A_eq=A, b_eq=b, bounds=[lam_bounds]*(w1_len*w2_len))
+    return np.sqrt(res.fun),res.x
+
